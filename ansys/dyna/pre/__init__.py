049dc684e1baaf7e795bf2829c85a126f1556f82
zhangzhanqun <zhanqun.zhang@ansys.com>
2022-06-28 13:56:31 +0800

    add sale demo

--- ansys/dyna/pre/__init__.py
+++ ansys/dyna/pre/__init__.py
@@ -5,3 +5,5 @@ from pydyna.dynaiga import DynaIGA
 from pydyna.dynaicfd import DynaICFD
 from pydyna.dynadem import DynaDEM
 from pydyna.dynaairbag import DynaAirbag
+from pydyna.dynasale import *
+from pydyna.dynamaterial import *
