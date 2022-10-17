69c4772ee2e6b3132e6340c285afcf62c9152d8d
zhangzhanqun <zhanqun.zhang@ansys.com>
2022-10-17 15:58:35 +0800

    update testing

--- tests/test_dynabase.py
+++ tests/test_dynabase.py
@@ -1,5 +1,18 @@
+import os
+import sys
 import pytest
 
-Class TestDynabase:
+sys.path.append(os.path.join(sys.path[0],os.pardir))
+from ansys.dyna.pre.dynasolution import *
+from ansys.dyna.pre.dynabase import *
+
     
-    test_
+def test_dynabase(initialfile,resolve_server_path):
+    solution = DynaSolution("localhost")
+    fns = []
+    fns.append(initialfile)
+    solution.open_files(fns)
+    solution.set_termination(0.03)
+    solution.set_output_database(abstat=2.0e-4,glstat=2.0e-4,matsum=2.0e-4,rcforc=2.0e-4,rbdout=2.0e-4,rwforc=2.0e-4)
+    solution.create_database_binary(dt=5e-4, ieverp=1)
+    solution.save_file()
