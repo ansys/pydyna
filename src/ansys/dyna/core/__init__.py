881b39599698d5be6bd9feb04af5ea6a1e2aca34
Roberto Pastor Muela <roberto.pastormuela@ansys.com>
2022-11-25 09:27:56 +0100

    Updating pre-commit calls

--- src/ansys/dyna/core/__init__.py
+++ src/ansys/dyna/core/__init__.py
@@ -1,7 +1,5 @@
+from ansys.dyna.core._version import __version__
+from ansys.dyna.core.solver.dynasolver import *
 import ansys.dyna.core.solver.dynasolver_pb2
 import ansys.dyna.core.solver.dynasolver_pb2_grpc
 import ansys.dyna.core.solver.grpc_tags
-
-from ansys.dyna.core._version import __version__
-from ansys.dyna.core.solver.dynasolver import *
-
