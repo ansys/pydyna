f936ad36603451e91f592eaa2bddcc4442cc785e
zhangzhanqun <zhanqun.zhang@ansys.com>
2022-11-28 16:23:31 +0800

    make examples follow the pyansys coding scheme

--- examples/EM/em_railgun.py
+++ examples/EM/em_railgun.py
@@ -16,56 +16,55 @@ from ansys.dyna.core.pre.dynamaterial import *
 from em_railgun_data import *
 from ansys.dyna.core.pre import examples
 
-if __name__ == "__main__":
-    hostname = "localhost"
-    if len(sys.argv) > 1:
-        hostname = sys.argv[1]
+hostname = "localhost"
+if len(sys.argv) > 1:
+    hostname = sys.argv[1]
 
-    em_solution = DynaSolution(hostname)
-    fns = []
-    path = examples.em_railgun+ os.sep
-    fns.append(path + "em_railgun.k")
-    em_solution.open_files(fns)
-    em_solution.set_termination(termination_time=3e-4)
-    em_solution.create_database_binary(dt=5e-6)
+em_solution = DynaSolution(hostname)
+fns = []
+path = examples.em_railgun+ os.sep
+fns.append(path + "em_railgun.k")
+em_solution.open_files(fns)
+em_solution.set_termination(termination_time=3e-4)
+em_solution.create_database_binary(dt=5e-6)
 
-    railgun = DynaEM()   
-    em_solution.add(railgun)
+railgun = DynaEM()   
+em_solution.add(railgun)
 
-    railgun.analysis.set_timestep(timestep=5e-6)
-    railgun.analysis.set_solver_bem(solver=BEMSOLVER.PCG)
-    railgun.analysis.set_solver_fem(solver=FEMSOLVER.DIRECT_SOLVER,relative_tol=1e-3)
-   
-    circuit = Circuit(circuit_type=CircuitType.IMPOSED_CURRENT_VS_TIME,loadcurve=Curve(x=[0,8e-5,2e-4,4e-4,6e-4,1e-3],y=[0,350,450,310,230,125],sfo=2e6))
-    circuit.set_current(current=SegmentSet(cur),current_inlet=SegmentSet(inlet),current_outlet=SegmentSet(outlet))
-    railgun.add(circuit)
+railgun.analysis.set_timestep(timestep=5e-6)
+railgun.analysis.set_solver_bem(solver=BEMSOLVER.PCG)
+railgun.analysis.set_solver_fem(solver=FEMSOLVER.DIRECT_SOLVER,relative_tol=1e-3)
 
-    matelastic = MatElastic(mass_density=2.64e-3,young_modulus=9.7e+10,poisson_ratio=0.31)
-    matelastic.set_electromagnetic_property(material_type=EMMATTYPE.CONDUCTOR,initial_conductivity=25)
-    matrigid = MatRigid(mass_density=2.64e-3,young_modulus=9.7e+10,poisson_ratio=0.31,center_of_mass_constraint=1,translational_constraint=7,rotational_constraint=7)
-    matrigid.set_electromagnetic_property(material_type=EMMATTYPE.CONDUCTOR,initial_conductivity=25)
-    
-    coil = SolidPart(1)
-    coil.set_material(matelastic)
-    coil.set_element_formulation(SolidFormulation.CONSTANT_STRESS_SOLID_ELEMENT)
-    railgun.parts.add(coil)
+circuit = Circuit(circuit_type=CircuitType.IMPOSED_CURRENT_VS_TIME,loadcurve=Curve(x=[0,8e-5,2e-4,4e-4,6e-4,1e-3],y=[0,350,450,310,230,125],sfo=2e6))
+circuit.set_current(current=SegmentSet(cur),current_inlet=SegmentSet(inlet),current_outlet=SegmentSet(outlet))
+railgun.add(circuit)
 
-    workpiece1 = SolidPart(2)
-    workpiece1.set_material(matrigid)
-    workpiece1.set_element_formulation(SolidFormulation.CONSTANT_STRESS_SOLID_ELEMENT)
-    railgun.parts.add(workpiece1)
+matelastic = MatElastic(mass_density=2.64e-3,young_modulus=9.7e+10,poisson_ratio=0.31)
+matelastic.set_electromagnetic_property(material_type=EMMATTYPE.CONDUCTOR,initial_conductivity=25)
+matrigid = MatRigid(mass_density=2.64e-3,young_modulus=9.7e+10,poisson_ratio=0.31,center_of_mass_constraint=1,translational_constraint=7,rotational_constraint=7)
+matrigid.set_electromagnetic_property(material_type=EMMATTYPE.CONDUCTOR,initial_conductivity=25)
 
-    workpiece2 = SolidPart(3)
-    workpiece2.set_material(matrigid)
-    workpiece2.set_element_formulation(SolidFormulation.CONSTANT_STRESS_SOLID_ELEMENT)
-    railgun.parts.add(workpiece2)
-    
-    railgun.boundaryconditions.create_spc(NodeSet(spc1),tx=False,ty=False,rz=False,death=0)
-    railgun.boundaryconditions.create_spc(NodeSet(spc2),tx=False,ty=False,rz=False,death=0)
+coil = SolidPart(1)
+coil.set_material(matelastic)
+coil.set_element_formulation(SolidFormulation.CONSTANT_STRESS_SOLID_ELEMENT)
+railgun.parts.add(coil)
 
-    contact = EMContact()
-    railgun.contacts.add(contact)
+workpiece1 = SolidPart(2)
+workpiece1.set_material(matrigid)
+workpiece1.set_element_formulation(SolidFormulation.CONSTANT_STRESS_SOLID_ELEMENT)
+railgun.parts.add(workpiece1)
 
-    railgun.set_rogowsky_coil_to_output_current(SegmentSet(cur))
-    
-    em_solution.save_file()
+workpiece2 = SolidPart(3)
+workpiece2.set_material(matrigid)
+workpiece2.set_element_formulation(SolidFormulation.CONSTANT_STRESS_SOLID_ELEMENT)
+railgun.parts.add(workpiece2)
+
+railgun.boundaryconditions.create_spc(NodeSet(spc1),tx=False,ty=False,rz=False,death=0)
+railgun.boundaryconditions.create_spc(NodeSet(spc2),tx=False,ty=False,rz=False,death=0)
+
+contact = EMContact()
+railgun.contacts.add(contact)
+
+railgun.set_rogowsky_coil_to_output_current(SegmentSet(cur))
+
+em_solution.save_file()
--- examples/ICFD/icfd_cylinderflow.py
+++ examples/ICFD/icfd_cylinderflow.py
@@ -13,56 +13,56 @@ from ansys.dyna.core.pre.dynasolution import *
 from ansys.dyna.core.pre.dynaicfd import *
 from ansys.dyna.core.pre import examples
 
-if __name__ == "__main__":
-    hostname = "localhost"
-    if len(sys.argv) > 1:
-        hostname = sys.argv[1]
 
-    icfd_solution = DynaSolution(hostname)
-    #Import the initial mesh data(nodes and elements)
-    fns = []
-    path = examples.icfd_cylinderflow+ os.sep
-    fns.append(path + "mesh.k")
-    icfd_solution.open_files(fns)
-    #Set total time of simulation  
-    icfd_solution.set_termination(termination_time=100) 
+hostname = "localhost"
+if len(sys.argv) > 1:
+    hostname = sys.argv[1]
 
-    icfd = DynaICFD()
-    icfd_solution.add(icfd)
+icfd_solution = DynaSolution(hostname)
+#Import the initial mesh data(nodes and elements)
+fns = []
+path = examples.icfd_cylinderflow+ os.sep
+fns.append(path + "mesh.k")
+icfd_solution.open_files(fns)
+#Set total time of simulation  
+icfd_solution.set_termination(termination_time=100) 
 
-    #define model
-    mat = MatICFD(flow_density=1.0,dynamic_viscosity=0.005)
+icfd = DynaICFD()
+icfd_solution.add(icfd)
 
-    part_inflow = ICFDPart(1)
-    part_inflow.set_material(mat)
-    part_inflow.set_prescribed_velocity(dof=ICFDDOF.X,motion=Curve(x=[0, 10000],y=[1, 1]))
-    part_inflow.set_prescribed_velocity(dof=ICFDDOF.Y,motion=Curve(x=[0, 10000],y=[0, 0]))
-    icfd.parts.add(part_inflow)
+#define model
+mat = MatICFD(flow_density=1.0,dynamic_viscosity=0.005)
 
-    part_outflow = ICFDPart(2)
-    part_outflow.set_material(mat)
-    part_outflow.set_prescribed_pressure(pressure = Curve(x=[0, 10000],y=[0, 0]))
-    icfd.parts.add(part_outflow)
+part_inflow = ICFDPart(1)
+part_inflow.set_material(mat)
+part_inflow.set_prescribed_velocity(dof=ICFDDOF.X,motion=Curve(x=[0, 10000],y=[1, 1]))
+part_inflow.set_prescribed_velocity(dof=ICFDDOF.Y,motion=Curve(x=[0, 10000],y=[0, 0]))
+icfd.parts.add(part_inflow)
 
-    part_symmetric = ICFDPart(3)
-    part_symmetric.set_material(mat)
-    part_symmetric.set_free_slip()
-    icfd.parts.add(part_symmetric)
+part_outflow = ICFDPart(2)
+part_outflow.set_material(mat)
+part_outflow.set_prescribed_pressure(pressure = Curve(x=[0, 10000],y=[0, 0]))
+icfd.parts.add(part_outflow)
 
-    part_wall= ICFDPart(4)
-    part_wall.set_material(mat)
-    part_wall.set_non_slip()
-    part_wall.compute_drag_force()
-    part_wall.set_boundary_layer(number=3)
-    icfd.parts.add(part_wall)
+part_symmetric = ICFDPart(3)
+part_symmetric.set_material(mat)
+part_symmetric.set_free_slip()
+icfd.parts.add(part_symmetric)
 
-    partvol = ICFDVolumePart(surfaces=[1, 2, 3, 4])
-    partvol.set_material(mat)  
-    icfd.parts.add(partvol)
-    # define the volume space that will be meshed,The boundaries 
-    #of the volume are the surfaces "spids"
-    meshvol = MeshedVolume(surfaces = [1, 2, 3, 4])
-    icfd.add(meshvol)
-    
-    icfd_solution.create_database_binary(dt=1)
-    icfd_solution.save_file()
+part_wall= ICFDPart(4)
+part_wall.set_material(mat)
+part_wall.set_non_slip()
+part_wall.compute_drag_force()
+part_wall.set_boundary_layer(number=3)
+icfd.parts.add(part_wall)
+
+partvol = ICFDVolumePart(surfaces=[1, 2, 3, 4])
+partvol.set_material(mat)  
+icfd.parts.add(partvol)
+# define the volume space that will be meshed,The boundaries 
+#of the volume are the surfaces "spids"
+meshvol = MeshedVolume(surfaces = [1, 2, 3, 4])
+icfd.add(meshvol)
+
+icfd_solution.create_database_binary(dt=1)
+icfd_solution.save_file()
