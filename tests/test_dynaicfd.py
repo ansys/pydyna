104683fd35e7f1df7c3a2bbbce2d21a9d2fa4c6b
zhangzhanqun <zhanqun.zhang@ansys.com>
2022-12-08 10:38:10 +0800

    add icfd model:plate flow

--- tests/test_dynaicfd.py
+++ tests/test_dynaicfd.py
@@ -11,7 +11,7 @@ from ansys.dyna.core.pre.dynaicfd import (
     ICFDVolumePart,
     MeshedVolume,
     ICFDAnalysis,
-    ICFD_SurfRemeshMethod
+    ICFD_SurfRemeshMethod,
 )
 
 
@@ -51,7 +51,8 @@ def test_icfd(icfd_initialfile, resolve_server_path, resolve_standard_path):
     outputfile = os.path.join(resolve_server_path, "output", "test_icfd.k")
     standardfile = os.path.join(resolve_standard_path, "icfd.k")
     assert comparefile(outputfile, standardfile)
-    
+
+
 def test_icfd_internal_3d_flow(resolve_icfd_path, resolve_server_path, resolve_standard_path):
     solution = DynaSolution("localhost")
     icfd_initialfile = os.path.join(resolve_icfd_path, "test_internal_3d_flow.k")
@@ -65,7 +66,7 @@ def test_icfd_internal_3d_flow(resolve_icfd_path, resolve_server_path, resolve_s
     icfdanalysis = ICFDAnalysis()
     icfdanalysis.set_timestep(timestep=0.05)
     icfdanalysis.set_volume_mesh(mesh_growth_scale_factor=1.1)
-    icfdanalysis.set_surface_mesh(remesh_method = ICFD_SurfRemeshMethod.LAPLACIAN_SMOOTHING)
+    icfdanalysis.set_surface_mesh(remesh_method=ICFD_SurfRemeshMethod.LAPLACIAN_SMOOTHING)
     icfd.add(icfdanalysis)
     # define model
     mat = MatICFD(flow_density=1.0, dynamic_viscosity=0.005)
@@ -101,5 +102,62 @@ def test_icfd_internal_3d_flow(resolve_icfd_path, resolve_server_path, resolve_s
     solution.create_database_binary(dt=1)
     solution.save_file()
     outputfile = os.path.join(resolve_server_path, "output", "test_internal_3d_flow.k")
-    standardfile = os.path.join(resolve_standard_path,"icfd","internal_3d_flow.k")
+    standardfile = os.path.join(resolve_standard_path, "icfd", "internal_3d_flow.k")
+    assert comparefile(outputfile, standardfile)
+
+
+def test_icfd_internal_3d_flow(resolve_icfd_path, resolve_server_path, resolve_standard_path):
+    solution = DynaSolution("localhost")
+    icfd_initialfile = os.path.join(resolve_icfd_path, "test_plate_flow.k")
+    fns = []
+    fns.append(icfd_initialfile)
+    solution.open_files(fns)
+
+    solution.set_termination(termination_time=100)
+    icfd = DynaICFD()
+    solution.add(icfd)
+
+    icfdanalysis = ICFDAnalysis()
+    icfdanalysis.set_timestep()
+    icfd.add(icfdanalysis)
+
+    # define model
+    mat = MatICFD(flow_density=1.0, dynamic_viscosity=0.005)
+
+    part_inflow = ICFDPart(1)
+    part_inflow.set_material(mat)
+    part_inflow.set_prescribed_velocity(dof=ICFDDOF.X, motion=Curve(x=[0, 10000], y=[1, 1]))
+    part_inflow.set_prescribed_velocity(dof=ICFDDOF.Y, motion=Curve(x=[0, 10000], y=[0, 0]))
+    icfd.parts.add(part_inflow)
+
+    part_outflow = ICFDPart(2)
+    part_outflow.set_material(mat)
+    part_outflow.set_prescribed_pressure(pressure=Curve(x=[0, 10000], y=[0, 0]))
+    icfd.parts.add(part_outflow)
+
+    part_symmetric = ICFDPart(3)
+    part_symmetric.set_material(mat)
+    part_symmetric.set_free_slip()
+    icfd.parts.add(part_symmetric)
+
+    part_wall = ICFDPart(4)
+    part_wall.set_material(mat)
+    part_wall.set_non_slip()
+    part_wall.compute_drag_force()
+    part_wall.set_boundary_layer(number=2)
+    icfd.parts.add(part_wall)
+
+    partvol = ICFDVolumePart(surfaces=[1, 2, 3, 4])
+    partvol.set_material(mat)
+    icfd.parts.add(partvol)
+    # define the volume space that will be meshed,The boundaries
+    # of the volume are the surfaces "spids"
+    meshvol = MeshedVolume(surfaces=[1, 2, 3])
+    meshvol.embed_shell([4])
+    icfd.add(meshvol)
+
+    solution.create_database_binary(dt=1)
+    solution.save_file()
+    outputfile = os.path.join(resolve_server_path, "output", "test_plate_flow.k")
+    standardfile = os.path.join(resolve_standard_path, "icfd", "plate_flow.k")
     assert comparefile(outputfile, standardfile)
