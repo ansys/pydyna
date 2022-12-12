7d91a313b0cb7c958b9de9f2039a724370a46f1f
zhangzhanqun <zhanqun.zhang@ansys.com>
2022-12-12 16:05:50 +0800

    add icfd model:driven cavity

--- tests/test_dynaicfd.py
+++ tests/test_dynaicfd.py
@@ -14,7 +14,10 @@ from ansys.dyna.core.pre.dynaicfd import (
     ICFD_SurfRemeshMethod,
     Gravity,
     GravityOption,
-    Compressible
+    Compressible,
+    ICFD_AnalysisType,
+    ICFD_MessageLevel
+
 )
 
 
@@ -395,4 +398,46 @@ def test_icfd_dam_break(resolve_icfd_path, resolve_server_path, resolve_standard
     solution.save_file()
     outputfile = os.path.join(resolve_server_path, "output", "test_dam_break.k")
     standardfile = os.path.join(resolve_standard_path, "icfd", "dam_break.k")
+    assert comparefile(outputfile, standardfile)
+
+def test_icfd_driven_cavity(resolve_icfd_path, resolve_server_path, resolve_standard_path):
+    solution = DynaSolution("localhost")
+    icfd_initialfile = os.path.join(resolve_icfd_path, "test_driven_cavity.k")
+    fns = []
+    fns.append(icfd_initialfile)
+    solution.open_files(fns)
+    icfd = DynaICFD()
+    solution.add(icfd)
+
+    icfdanalysis = ICFDAnalysis()
+    icfdanalysis.set_type(analysis_type=ICFD_AnalysisType.STEADY_STATE_ANALYSIS)
+    icfdanalysis.set_output(messagelevel=ICFD_MessageLevel.FULL_OUTPUT_INFORMATION,iteration_interval=250)
+    icfdanalysis.set_steady_state(max_iteration=2500,momentum_tol_limit=1e-8,pressure_tol_limit=1e-8,velocity_relax_param=1,pressure_relax_param=1)
+    icfd.add(icfdanalysis)
+
+    # define model
+    mat = MatICFD(flow_density=1, dynamic_viscosity=0.001)
+
+    part1 = ICFDPart(1)
+    part1.set_material(mat)
+    part1.set_prescribed_velocity(dof=ICFDDOF.X, motion=Curve(x=[0, 10000], y=[1, 1]))
+    icfd.parts.add(part1)
+
+    part2 = ICFDPart(2)
+    part2.set_material(mat)
+    part2.set_non_slip()
+    icfd.parts.add(part2)
+
+    partvol = ICFDVolumePart(surfaces=[1, 2])
+    partvol.set_material(mat)
+    icfd.parts.add(partvol)
+    # define the volume space that will be meshed,The boundaries
+    # of the volume are the surfaces "spids"
+    meshvol = MeshedVolume(surfaces=[1, 2])
+    icfd.add(meshvol)
+
+    solution.create_database_binary(dt=250)
+    solution.save_file()
+    outputfile = os.path.join(resolve_server_path, "output", "test_driven_cavity.k")
+    standardfile = os.path.join(resolve_standard_path, "icfd", "driven_cavity.k")
     assert comparefile(outputfile, standardfile)
