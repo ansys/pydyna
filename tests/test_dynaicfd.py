fa7eb0e940d7e692369f2c8ac38d6bc516f67cc1
zhangzhanqun <zhanqun.zhang@ansys.com>
2022-12-29 15:14:26 +0800

    add icfd model:strong fsi

--- tests/test_dynaicfd.py
+++ tests/test_dynaicfd.py
@@ -520,6 +520,73 @@ def test_icfd_weak_fsi(resolve_icfd_path, resolve_server_path, resolve_standard_
     standardfile = os.path.join(resolve_standard_path, "icfd", "weak_fsi.k")
     assert comparefile(outputfile, standardfile)
 
+def test_icfd_strong_fsi(resolve_icfd_path, resolve_server_path, resolve_standard_path):
+    solution = DynaSolution("localhost")
+    icfd_initialfile = os.path.join(resolve_icfd_path, "test_strong_fsi.k")
+    fns = []
+    fns.append(icfd_initialfile)
+    solution.open_files(fns)
+    solution.set_termination(termination_time=40)
+    icfd = DynaICFD()
+    solution.add(icfd)
+
+    icfdanalysis = ICFDAnalysis()
+    icfdanalysis.set_timestep(0.05)
+    icfd.add(icfdanalysis)
+
+    icfd.implicitanalysis.set_initial_timestep_size(
+        size=10
+    )
+    icfd.implicitanalysis.set_dynamic(gamma=0.6, beta=0.4)
+    icfd.implicitanalysis.set_solution(
+        iteration_limit=100, stiffness_reformation_limit=150
+    )
+
+    # define model
+    mat = MatICFD(flow_density=1.0, dynamic_viscosity=0.005)
+    part_inflow = ICFDPart(1)
+    part_inflow.set_material(mat)
+    part_inflow.set_prescribed_velocity(dof=ICFDDOF.X, motion=Curve(x=[0, 5, 6, 10000], y=[0, 0, 1, 1]))
+    part_inflow.set_prescribed_velocity(dof=ICFDDOF.Y, motion=Curve(x=[0, 10000], y=[0, 0]))
+    icfd.parts.add(part_inflow)
+    part_outflow = ICFDPart(2)
+    part_outflow.set_material(mat)
+    part_outflow.set_prescribed_pressure(pressure=Curve(x=[0, 10000], y=[0, 0]))
+    icfd.parts.add(part_outflow)
+    part_symmetric = ICFDPart(3)
+    part_symmetric.set_material(mat)
+    part_symmetric.set_free_slip()
+    icfd.parts.add(part_symmetric)
+    part_wall = ICFDPart(4)
+    part_wall.set_material(mat)
+    part_wall.set_non_slip()
+    part_wall.set_fsi()
+    part_wall.compute_drag_force()
+    part_wall.set_boundary_layer(number=3)
+    icfd.parts.add(part_wall)
+
+    partvol = ICFDVolumePart(surfaces=[1, 2, 3, 4])
+    partvol.set_material(mat)
+    icfd.parts.add(partvol)
+    # define the volume space that will be meshed,The boundaries
+    # of the volume are the surfaces "spids"
+    meshvol = MeshedVolume(surfaces=[1, 2, 3, 4])
+    icfd.add(meshvol)
+
+    #define rigid cylinder
+    matrigid = MatRigid(mass_density=1.2,young_modulus=2e11,poisson_ratio=0.3)
+    cylinder = ShellPart(1)
+    cylinder.set_material(matrigid)
+    cylinder.set_element_formulation(ShellFormulation.PLANE_STRESS)
+    icfd.parts.add(cylinder)
+    # Define boundary conddition
+    icfd.boundaryconditions.create_imposed_motion(PartSet([1]),Curve(func="2*3.14/10*sin(2*3.14/10*TIME+3.14/2)"),dof=DOF.Y_TRANSLATIONAL,motion=Motion.VELOCITY)
+    solution.create_database_binary(dt=0.1)
+    solution.save_file()
+    outputfile = os.path.join(resolve_server_path, "output", "test_strong_fsi.k")
+    standardfile = os.path.join(resolve_standard_path, "icfd", "strong_fsi.k")
+    assert comparefile(outputfile, standardfile)
+
 def test_icfd_dem_coupling(resolve_icfd_path, resolve_server_path, resolve_standard_path):
     solution = DynaSolution("localhost")
     icfd_initialfile = os.path.join(resolve_icfd_path, "test_dem_coupling.k")
