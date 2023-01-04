7e7cbc92292d54968f797cefa980daf2f48e5a6f
zhangzhanqun <zhanqun.zhang@ansys.com>
2023-01-04 14:39:42 +0800

    add icfd model:mesh morphing

--- tests/test_dynaicfd.py
+++ tests/test_dynaicfd.py
@@ -702,6 +702,70 @@ def test_icfd_mesh_adaptivity(resolve_icfd_path, resolve_server_path, resolve_st
     standardfile = os.path.join(resolve_standard_path, "icfd", "mesh_adaptivity.k")
     assert comparefile(outputfile, standardfile)
 
+def test_icfd_mesh_morphing(resolve_icfd_path, resolve_server_path, resolve_standard_path):
+    solution = DynaSolution("localhost")
+    icfd_initialfile = os.path.join(resolve_icfd_path, "test_mesh_morphing.k")
+    fns = []
+    fns.append(icfd_initialfile)
+    solution.open_files(fns)
+    solution.set_termination(termination_time=40)
+    icfd = DynaICFD()
+    solution.add(icfd)
+
+    icfdanalysis = ICFDAnalysis()
+    icfdanalysis.set_timestep(0.05)
+    icfd.add(icfdanalysis)
+
+    # define model
+    mat = MatICFD(flow_density=1.0, dynamic_viscosity=0.005)
+
+    part_inflow = ICFDPart(1)
+    part_inflow.set_material(mat)
+    part_inflow.set_prescribed_velocity(dof=ICFDDOF.X, motion=Curve(x=[0, 5, 6, 10000], y=[0, 0, 1, 1]))
+    part_inflow.set_prescribed_velocity(dof=ICFDDOF.Y, motion=Curve(x=[0, 10000], y=[0, 0]))
+    icfd.parts.add(part_inflow)
+
+    part_outflow = ICFDPart(2)
+    part_outflow.set_material(mat)
+    part_outflow.set_prescribed_pressure(pressure=Curve(x=[0, 10000], y=[0, 0]))
+    icfd.parts.add(part_outflow)
+
+    part_symmetric = ICFDPart(3)
+    part_symmetric.set_material(mat)
+    part_symmetric.set_free_slip()
+    icfd.parts.add(part_symmetric)
+
+    part_wall = ICFDPart(4)
+    part_wall.set_material(mat)
+    part_wall.set_non_slip()
+    part_wall.compute_drag_force()
+    part_wall.set_boundary_layer(number=2)
+    part_wall.set_imposed_move(vy=Curve(func="2*3.14/10*sin(2*3.14/10*TIME+3.14/2)"))
+    icfd.parts.add(part_wall)
+
+    part_meshmorph = ICFDPart(5)
+    part_meshmorph.set_material(mat)
+    icfd.parts.add(part_meshmorph)
+
+    partvol1 = ICFDVolumePart(surfaces=[1, 2, 3, 5])
+    partvol1.set_material(mat)
+    icfd.parts.add(partvol1)
+    partvol2 = ICFDVolumePart(surfaces=[5, 4])
+    partvol2.set_material(mat)
+    partvol2.set_imposed_move(vy=Curve(func="2*3.14/10*sin(2*3.14/10*TIME+3.14/2)"))
+    icfd.parts.add(partvol2)
+    # define the volume space that will be meshed,The boundaries
+    # of the volume are the surfaces "spids"
+    meshvol = MeshedVolume(surfaces=[1, 2, 3, 4])
+    meshvol.set_fluid_interfaces([5])
+    icfd.add(meshvol)
+
+    solution.create_database_binary(dt=0.5)
+    solution.save_file()
+    outputfile = os.path.join(resolve_server_path, "output", "test_mesh_morphing.k")
+    standardfile = os.path.join(resolve_standard_path, "icfd", "mesh_morphing.k")
+    assert comparefile(outputfile, standardfile)
+
 def test_icfd_dem_coupling(resolve_icfd_path, resolve_server_path, resolve_standard_path):
     solution = DynaSolution("localhost")
     icfd_initialfile = os.path.join(resolve_icfd_path, "test_dem_coupling.k")
