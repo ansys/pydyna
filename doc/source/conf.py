a0d725f56b2190622c21367bc7d811a0c6e79e4b
Roberto Pastor Muela <37798125+RobPasMue@users.noreply.github.com>
2023-03-07 08:32:15 +0100

    Update conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -132,7 +132,7 @@ html_context = {
 }
 html_theme_options = {
     "switcher": {
-        "json_url": f"https://{cname}/release/versions.json",
+        "json_url": f"https://{cname}/versions.json",
         "version_match": get_version_match(__version__),
     },
     "check_switcher": False,
