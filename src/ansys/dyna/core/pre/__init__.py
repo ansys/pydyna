fe2302ad303ccae09825c17c0e3f152dbe3af88d
zhangzhanqun <zhanqun.zhang@ansys.com>
2023-03-22 09:48:03 +0800

    checking code style

--- src/ansys/dyna/core/pre/__init__.py
+++ src/ansys/dyna/core/pre/__init__.py
@@ -2,21 +2,12 @@
 import logging
 import os
 
-import appdirs
-
 from ansys.dyna.core.pre.dynalogging import Logger
 
 LOG = Logger(level=logging.ERROR, to_file=False, to_stdout=True)
 LOG.debug("Loaded logging module as LOG")
 
 _LOCAL_PORTS = []
-
-try:
-    import importlib.metadata as importlib_metadata
-except ModuleNotFoundError:  # pragma: no cover
-    import importlib_metadata
-
-# __version__ = importlib_metadata.version(__name__.replace(".", "-"))
 
 from .dynabase import *
 from .dynadem import DynaDEM
--- src/ansys/dyna/core/pre/launcher.py
+++ src/ansys/dyna/core/pre/launcher.py
@@ -6,10 +6,7 @@ import subprocess
 import threading
 
 from ansys.dyna.core.pre import LOG
-from ansys.dyna.core.pre.misc import (
-    check_valid_ip,
-    check_valid_port,
-)
+from ansys.dyna.core.pre.misc import check_valid_ip, check_valid_port
 
 LOCALHOST = "127.0.0.1"
 DYNAPRE_DEFAULT_PORT = 50051
@@ -39,18 +36,14 @@ def port_in_use(port, host=LOCALHOST):
             return True
 
 
-def launch_grpc(
-    port=DYNAPRE_DEFAULT_PORT,
-    ip=LOCALHOST,
-    server_path = None
-) -> tuple:  # pragma: no cover
+def launch_grpc(port=DYNAPRE_DEFAULT_PORT, ip=LOCALHOST, server_path=None) -> tuple:  # pragma: no cover
     """Start kwserver locally in gRPC mode.
     Parameters
     ----------
     port : int
         Port to launch PyDyna gRPC on.  Final port will be the first
         port available after (or including) this port.
-    
+
     Returns
     -------
     int
@@ -58,15 +51,15 @@ def launch_grpc(
 
     """
     LOG.debug("Starting 'launch_kwserver'.")
-    
+
     command = "python kwserver.py"
     LOG.debug(f"Starting kwserver with command: {command}")
 
-    #env_vars = update_env_vars(add_env_vars, replace_env_vars)
+    # env_vars = update_env_vars(add_env_vars, replace_env_vars)
     LOG.info(f"Running in {ip}:{port} the following command: '{command}'")
 
     LOG.debug("kwserver starting in background.")
-    process = subprocess.Popen(['python','kwserver.py'],cwd = server_path ,shell=True)
+    process = subprocess.Popen(["python", "kwserver.py"], cwd=server_path, shell=True)
     process.wait()
     # while True:
     #     pass
@@ -74,7 +67,7 @@ def launch_grpc(
 
 
 class ServerThread(threading.Thread):
-    def __init__(self,threadID,port,ip,server_path):
+    def __init__(self, threadID, port, ip, server_path):
         threading.Thread.__init__(self)
         self.threadID = threadID
         self.port = port
@@ -83,7 +76,7 @@ class ServerThread(threading.Thread):
         self.process = None
 
     def run(self):
-        self.process = launch_grpc(ip=self.ip,port=self.port,server_path=self.server_path)
+        self.process = launch_grpc(ip=self.ip, port=self.port, server_path=self.server_path)
 
     def termination(self):
         self.process.termination()
@@ -97,7 +90,7 @@ def launch_dynapre(
     Parameters
     ----------
     port : int
-        Port to launch MAPDL gRPC on. 
+        Port to launch MAPDL gRPC on.
     ip : bool, optional
         Specify the IP address of the PyDyna instance to connect to.
         You can also provide a hostname as an alternative to an IP address.
@@ -115,7 +108,7 @@ def launch_dynapre(
     if (ip.lower() == "localhost" or ip == LOCALHOST) and not DynaSolution.grpc_local_server_on():
         LOG.debug("Starting kwserver")
         server_path = os.path.join(os.getcwd(), "../../src/ansys/dyna/core/pre/Server")
-        threadserver = ServerThread(1,port=port,ip=ip,server_path=server_path)
+        threadserver = ServerThread(1, port=port, ip=ip, server_path=server_path)
         threadserver.setDaemon(True)
         threadserver.start()
 
@@ -126,9 +119,10 @@ def launch_dynapre(
 
     # return dynasln
 
+
 if __name__ == "__main__":
     server_path = os.path.join(os.getcwd(), "Server")
-    process = subprocess.Popen(['python','kwserver.py'],cwd = server_path ,shell=True)
+    process = subprocess.Popen(["python", "kwserver.py"], cwd=server_path, shell=True)
     process.wait()
     process.terminate()
     print(process)
