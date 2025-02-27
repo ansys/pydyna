b49bc3d425decb482151c0faf9b0c21b652da17f
Raphael Heiniger <136489399+RaphaelHeiniger@users.noreply.github.com>
2025-02-27 14:28:35 +0100

    fix: remove print statement from test_initial_temperature (#757)
    Co-authored-by: pyansys-ci-bot <92810346+pyansys-ci-bot@users.noreply.github.com>

--- tests/test_keywords.py
+++ tests/test_keywords.py
@@ -410,8 +410,6 @@ def test_initial_temperature(ref_string):
     assert tin.write() == ref_string.test_initial_temperature_node_string
     tis = kwd.InitialTemperatureSet()
     tis.loads(ref_string.test_initial_temperature_set_string)
-    print(tis.write())
-    print(ref_string.test_initial_temperature_set_string)
     assert tis.write() == ref_string.test_initial_temperature_set_string
 
 
