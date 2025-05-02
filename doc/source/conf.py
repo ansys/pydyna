d6cf8dd993991c952af353c1c7fb335d85ab350e
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2025-05-02 16:11:55 +0200

    fix: suppress numpydoc warnings

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -59,11 +59,11 @@ numpydoc_validation_checks = {
     # Summary
     "SS01",  # No summary found
     "SS02",  # Summary does not start with a capital letter
-    "SS03",  # Summary does not end with a period
+    #"SS03",  # Summary does not end with a period
     "SS04",  # Summary contains heading whitespaces
     "SS05",  # Summary must start with infinitive verb, not third person
     # Parameters
-    "PR10",  # Parameter "{param_name}" requires a space before the colon '
+    #"PR10",  # Parameter "{param_name}" requires a space before the colon '
     # separating the parameter name and type",
 }
 
