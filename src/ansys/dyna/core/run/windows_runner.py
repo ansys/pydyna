b0671bf9afe50b3e84ad5d5a6cda16140c8aa7e6
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2025-06-03 14:34:43 +0200

    fix: codestyle

--- src/ansys/dyna/core/run/windows_runner.py
+++ src/ansys/dyna/core/run/windows_runner.py
@@ -24,7 +24,6 @@
 
 import os
 from pathlib import Path
-import shutil
 import subprocess
 
 from ansys.tools.path import get_latest_ansys_installation
@@ -141,16 +140,7 @@ class WindowsRunner(BaseRunner):
             else:
                 print("[WARNING] lsrun.out.txt not found!")
             raise RuntimeWarning(f"LS-DYNA run failed with error: {e.stderr}") from e
-        
-        
-    # def set_input_file(self) -> None:
-    #     """set the input file in the working directory if it does not exist."""
-    #     input_file =os.path.basename(self.input_file)
-    #     dest_path = os.path.join(self.working_directory, input_file)
-    #     if not os.path.isfile(dest_path):
-    #         shutil.copyfile(self.input_file, dest_path)
-        
-        
+
     def _get_command_line(self) -> str:
         """Get the command line to run LS-DYNA."""
         script = f'call "{self._get_env_script()}"'
