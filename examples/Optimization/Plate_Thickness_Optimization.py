664818ca1dca4d7aa9ec90922e2c4e885d3b7a1a
Revathyvenugopal162 <revathy.venugopal@ansys.com>
2025-08-26 13:27:12 +0200

    fix: update the plot

--- examples/Optimization/Plate_Thickness_Optimization.py
+++ examples/Optimization/Plate_Thickness_Optimization.py
@@ -386,6 +386,11 @@ for res in all_results:
     color = "r" if max(max_disp_data) <= target_displacement else "b"
     label = f"Thickness {thickness:.4f}"
     plt.plot(time_data, max_disp_data, color=color, label=label)
+    plt.xlabel("Time (ms)")
+    plt.ylabel("Displacement (mm)")
+    plt.title("Plate Displacement vs Time for Different Thicknesses")
+    plt.grid(True)
+    plt.show()
 
 ###############################################################################
 # Generate graphical output
@@ -393,8 +398,8 @@ for res in all_results:
 #
 
 
-plt.xlabel("Time (ms)")
-plt.ylabel("Displacement (mm)")
-plt.title("Plate Displacement vs Time for Different Thicknesses")
-plt.grid(True)
-plt.show()
+# plt.xlabel("Time (ms)")
+# plt.ylabel("Displacement (mm)")
+# plt.title("Plate Displacement vs Time for Different Thicknesses")
+# plt.grid(True)
+# plt.show()
